b0152eed59ae7fb84e50ae5456ac75330b500d1f
Vasiliauskas <vidasvasiliauskas@gmail.com>
2016-02-14 22:56:05 +0200

    Edited powershell scrip

--- nuget/pack.ps1
+++ nuget/pack.ps1
@@ -1,10 +1,11 @@
-﻿$version = [System.Reflection.Assembly]::LoadFile("..\bin\Build\Gappalytics.dll").GetName().Version
+﻿$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+$version = [System.Reflection.Assembly]::LoadFile("$root\bin\Build\Gappalytics.dll").GetName().Version
 $versionStr = "{0}.{1}.{2}" -f ($version.Major, $version.Minor, $version.Build)
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content Gappalytics.nuspec)
+$content = (Get-Content $root\nuget\Gappalytics.nuspec)
 $content = $content -replace '\$version\$',$versionStr
 
-$content | Out-File Gappalytics.compiled.nuspec
-& nuget.exe pack Gappalytics.compiled.nuspec
+$content | Out-File $root\nuget\Gappalytics.compiled.nuspec 
+& $root\nuget\nuget.exe pack $root\nuget\Gappalytics.compiled.nuspec
