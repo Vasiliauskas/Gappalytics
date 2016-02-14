0c2a068f6cc27df6a87b60125900beda592d7945
Vasiliauskas <vidasvasiliauskas@gmail.com>
2016-02-14 23:06:04 +0200

    fixing ps script

--- nuget/pack.ps1
+++ nuget/pack.ps1
@@ -8,4 +8,4 @@ $content = (Get-Content $root\nuget\Gappalytics.nuspec)
 $content = $content -replace '\$version\$',$versionStr
 
 $content | Out-File $root\nuget\Gappalytics.compiled.nuspec 
-$root\nuget\nuget.exe pack $root\nuget\Gappalytics.compiled.nuspec
+&$root\nuget\nuget.exe pack $root\nuget\Gappalytics.compiled.nuspec
