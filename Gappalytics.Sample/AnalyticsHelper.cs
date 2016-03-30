c0d670cf735e866030ab6678f861524eec144ed8
Vasiliauskas <vidasvasiliauskas@gmail.com>
2016-03-30 22:50:50 +0300

    Added sample project correctly

---
+++ Gappalytics.Sample/AnalyticsHelper.cs
@@ -0,0 +1,59 @@
+﻿using Gappalytics.Core;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gappalytics.Sample
+{
+    public class AnalyticsHelper
+    {
+        private IAnalyticsSession _analyticsSession;
+
+        private static AnalyticsHelper _Current;
+        public static AnalyticsHelper Current
+        {
+            get
+            {
+                if (_Current == null)
+                    Initialize();
+                return _Current;
+            }
+        }
+
+        public bool AnalyticsEnabled { get; set; }
+        public bool IsTrial { get; set; }
+
+        /// <summary>
+        /// This method displays how to use non persistent mode of Gappalytics, each time you run will make a new unique user on GA dashboard
+        /// For persistent mode, store first visit date, visit count (increment each time), and random number somewhere like app settings
+        /// Use Constructor overload for Analytics sessions 
+        /// </summary>
+        private static void Initialize()
+        {
+            _Current = new AnalyticsHelper();
+
+            // Replace UA code with your Google Analytics Tracking code
+            _Current._analyticsSession = new AnalyticsSession("", "UA-XXXXXXX-X");
+        }
+
+
+        public void LogEvent(string eventName, string url)
+        {
+            if (string.IsNullOrEmpty(eventName) || string.IsNullOrEmpty(url))
+                throw new InvalidOperationException("Parameters cannot contain empty values");
+            try
+            {
+                var request = _analyticsSession.CreatePageViewRequest(url, eventName);
+                request.Send();
+            }
+            catch (Exception ex)
+            {
+                ex.ToString();
+                // Keep silent exceptions for robust scenarios
+            }
+        }
+    }
+}
--- Gappalytics.Sample/MainWindow.xaml.cs
+++ Gappalytics.Sample/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApplication1
+namespace Gappalytics.Sample
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,6 +23,16 @@ namespace WpfApplication1
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AnalyticsHelper.Current.LogEvent("Just some event text", @"/SampleApp/TestButton1/");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AnalyticsHelper.Current.LogEvent("Just some other event text", @"/SampleApp/TestButton2/SubAction");
         }
     }
 }
